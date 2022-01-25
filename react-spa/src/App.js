bc7919031898ea00ab5463dc8261fae26b083368
Patrick Murray <patrick.d.murray@kp.org>
2022-01-24 16:15:41 -0800

    enable dev serving

--- react-spa/src/App.js
+++ react-spa/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+let DEV_URL = '';
+if (process.env.NODE_ENV === 'development') {
+  DEV_URL = 'http://localhost:3000';
+}
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+this.state = {
+    users: []
+  };
+}
+
+async componentDidMount() {
+  // Call self-hosted API to get users response
+  const res = await fetch(`${DEV_URL}/users`);
+  const users = await res.json();
+  this.setState({
+    users
+  });
+}
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <div className="App">
+    {this.state.users.map(user => (
+     <p>{user}</p>
+    ))}
+   </div>
+   );
+  }
 }
 
 export default App;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET React App */
+router.get('/app', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public', 'app.html'));
 });
 
 module.exports = router;
