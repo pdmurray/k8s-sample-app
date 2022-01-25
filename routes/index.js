24ded442a8b6007325efaee62e0ed1a506cee22c
Patrick Murray <patrick.d.murray@kp.org>
2022-01-24 16:30:12 -0800

    serve react app on root

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
+/* Commented out so that express will not serve the index, react will.
+   The express API will still be available on /users.
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
+
 
 /* GET React App */
-router.get('/app', function(req, res, next) {
+// Use the first version here to instead serve the react app on /app.
+
+//router.get(['/app', '/app/*'], function(req, res, next) {
+router.get(['/'], function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public', 'app.html'));
 });
 
